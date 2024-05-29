e32e940f62e870e98aa4f3be54848fda77925b23
Wingman <wucx0618@outlook.com>
2024-05-29 23:14:49 +0100

    Deploy the real-time mel spectrogram generate function and converting the amplitude to db

--- anduino_deployments/kws/Kws/Kws.ino
+++ anduino_deployments/kws/Kws/Kws.ino
@@ -1,7 +1,7 @@
 #include <TensorFlowLite.h>
 #include <PDM.h>
 #include <Complex.h>
-
+#include <cmath>
 #include "test_input.h"
 
 #include "Ml_model.h"
@@ -19,12 +19,19 @@ namespace{
   short sampleBuffer[256];
   volatile int samplesRead;
   int8_t inputBuffer[512];
-  double* inputBuffer_hamming = nullptr;
-  double* result = nullptr;
-  bool is_full = false;
+  // pointer for single fft
   int pointer = 0;
+  // pointer for stft flatten
+  int pointer_2 = 0;
   int step_size = 256;
   int window_size = 512;
+  int n_mels = 40; 
+//  int input_test[512];
+  double* inputBuffer_hamming = nullptr;
+  double* rfft = nullptr;
+  double* mel_spectrogram = nullptr;
+  double* model_input_mel_db = nullptr;
+  double model_input_mel[2400];
   
   //Tensorflow namespace
   tflite::ErrorReporter *error_reporter = nullptr;
@@ -39,6 +46,7 @@ namespace{
 }
 
 void setup() {
+  Serial.begin(9600);
   // Setup tensorflow lite micro
   static tflite::MicroErrorReporter micro_error_reporter;
   error_reporter = &micro_error_reporter;
@@ -78,7 +86,7 @@ void setup() {
   static tflite::MicroInterpreter static_interpreter(
     model, micro_op_resolver, tensor_arena, kTensorArenaSize, error_reporter);
   interpreter = &static_interpreter;
-/*
+
   TfLiteStatus allocate_status = interpreter->AllocateTensors();
   if(allocate_status != kTfLiteOk){
     TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
@@ -90,7 +98,7 @@ void setup() {
   model_input_buffer = model_input->data.int8;
 
   model_output = interpreter->output(0);
-*/
+
   // Setup microphone
   // Reference: https://docs.arduino.cc/learn/built-in-libraries/pdm/ 
   PDM.onReceive(onPDMdata);
@@ -100,22 +108,29 @@ void setup() {
 }
 
 void loop() {
-/*
-  double* input_test_hamming = hamming_data(input_test_sin, 16);
-  double* result = dft_abs(input_test_hamming, 16);
-  for(int i=0; i<16; i++){
-    Serial.print(result[i]);
-    Serial.print(" ");
+  if(pointer_2 == 2400){
+//    for(int i=0; i<2400; i++){
+//      Serial.print(model_input_mel[i]);
+//      Serial.print(" ");
+//    }
+    model_input_mel_db = amplitude_to_db(model_input_mel, 2400);
+    for(int i=0; i<2360; i++){
+      model_input_mel[i] = model_input_mel[i+40];
+    }
+    pointer_2 = 2360;
   }
-  Serial.print("\n");
-*/
-
+  
   if(pointer == window_size){
-    inputBuffer_hamming = hamming_data(inputBuffer, 512);
-    result = dft_abs(inputBuffer_hamming, 512);
-    for(int i=0; i<512; i++){
-      Serial.print(result[i]);
-      Serial.print(" ");
+    inputBuffer_hamming = hamming_data(inputBuffer, window_size);
+    rfft = rfft_abs(inputBuffer_hamming, window_size);
+    mel_spectrogram = mel_spec(rfft, n_mels);
+//    for(int i=0; i<n_mels; i++){
+//      Serial.print(mel_spectrogram[i]);
+//      Serial.print(" ");
+//    }
+    for(int i=0; i<n_mels; i++){
+      model_input_mel[pointer_2] = mel_spectrogram[i];
+      pointer_2 += 1;
     }
     for(int i=0; i<step_size; i++){
       inputBuffer[i] = inputBuffer[i+256];
@@ -132,11 +147,8 @@ void loop() {
     samplesRead = 0;
   }
 
-  free(inputBuffer_hamming);
-  free(result);
-/*
 // Faking inputs
-  for(int i=0; i<15872; i++){
+  for(int i=0; i<2400; i++){
     model_input_buffer[i] = input_val[i];
   }
 
@@ -150,9 +162,13 @@ void loop() {
 
   OutputCommands(data_no, data_yes);
 
-*/
+
+  free(inputBuffer_hamming);
+  free(rfft);
+  free(mel_spectrogram);
+  free(model_input_mel_db);
   Serial.println("one loop");
-  delay(5000);
+//  delay(1000);
 }
 
 void onPDMdata(){
