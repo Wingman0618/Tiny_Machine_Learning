8bdc843dbc920da75ee0b4b87c7553676a024117
Wingman <wucx0618@outlook.com>
2024-06-03 21:13:24 +0100

    delete the anduino_deployments

--- anduino_deployments/kws/Kws/Output_analysis.h
+++
@@ -1,4 +0,0 @@
-#include "stdint.h"
-#include "Arduino.h"
-
-void OutputCommands(int8_t data_0, int8_t data_1);
--- anduino_deployments/kws/Kws/test_input.h
+++
@@ -1,4 +0,0 @@
-#include "stdint.h"
-extern const int8_t input_val[];
-extern const int input_val_2[];
-extern double input_test_sin[];
